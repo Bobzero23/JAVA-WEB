42f59957bdfc6a3f4bf9c91675c9d6fb0cba5502
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-08-02 15:28:50 +0300

    COMMIT 1528-2

--- src/main/java/com/bobzero/Servlet_config_context.java
+++ src/main/java/com/bobzero/Servlet_config_context.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class Servlet_config_context extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
