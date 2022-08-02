ccad720082109a946bc885989e83385153dc6105
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-08-02 14:22:36 +0300

    COMMIT 1423-2

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,4 +1,11 @@
-
-public class Notes {
-
-}
+/* INTRODUCTION TO JSP AND SERVLETS 
+ * 
+ * I assume you already set up the <Tomcat> and created a <Web Daynamic Project>
+ * MAKE SURE THE WEB PROJECT YOU CREATED HAS <web.xml> FILE IN IT OR CREATE AGAIN
+ * after giving a name to your web daynamic project DONT click FINISH 
+ * click <next> until you find the <generate a web.xml file> option and check it 
+ * 
+ * THINGIS YOU SHOULD KNOW FOR NOW
+ * 
+ * 
+ */
