19cfea27e1d1cdc1e10aa08110bec27768706baf
Bobzero23 <rajabu.mustafa.00@gmail.com>
2022-08-02 17:12:27 +0300

    COMMIT 1712-2

--- src/main/java/com/bobzero/HelloWorld.java
+++ src/main/java/com/bobzero/HelloWorld.java
@@ -8,24 +8,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/*main class*/
 @SuppressWarnings("serial")
-public class Addition extends HttpServlet {
+public class HelloWorld extends HttpServlet{
 
-	/* this is the only method with class HttpServlet for now*/
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-
-		/*fetching the values from html file*/
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
-		
-		/*calculation*/
-		int k = (j + i);
-		
-		/*displaying results*/
 		PrintWriter out = res.getWriter();
-		out.println("The result of addition is : " + k);
-		
+		out.println("Hello Bobzero");
 	}
-
 }
--- src/main/java/com/bobzero/Notes.java
+++ src/main/java/com/bobzero/Notes.java
@@ -3,9 +3,12 @@
  * I assume you already set up the <Tomcat> and created a <Web Daynamic Project>
  * MAKE SURE THE WEB PROJECT YOU CREATED HAS <web.xml> FILE IN IT OR CREATE AGAIN
  * after giving a name to your web daynamic project DONT click FINISH 
- * click <next> until you find the <generate a web.xml file> option and check it 
- * 
- * THINGIS YOU SHOULD KNOW FOR NOW
- * 
- * 
+ * click <next> until you find the <generate a web.xml file> option and check it  
  */
+
+/*STRING QUERY & DOGET & DOPOST
+ * you have probably noticed that whatever you do appears on he url
+ * which called STRING QUERY to avoid that we can use doGet or doPost methods
+ * DOGET is used whenever you wanna get data from the client
+ * DOPOST is used whenever you wanna post data to client
+ * */
