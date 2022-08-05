f3e289a4a0c622e8d2a7050cee3bb0d38d9d50ac
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-05 15:37:50 +0300

    COMMIT 1537-5

--- src/main/java/jsp/Notes.java
+++ src/main/java/jsp/Notes.java
@@ -29,6 +29,17 @@
  *  The list of the names of these brackets by order scriplet, declaration, directive, expression.
  *  
  *  
+ *  THERE ARE MANY THINGS TO WORK WITH BUT I WILL EXPLAIN BY TIME AS I LEARN
+ *  
+ *  EXCEPTION HANDLING
+ *  the best way to handle the excption is to create an ERROR PAGE, this is just a normal jsp file 
+ *  whichi we will use to handle any error in other files.. is better to define it as an error page 
+ *  and to do that simply in DIRECTIVE TAG say isErrorPage = "true" then you can the exception object inside the
+ *  expression tag like this <%= exception.getMassage()%> to print any error. btw if you do initiate as an
+ *  error page these object wont work for this file 
+ *  
+ *  
+ *  
  *  
  *  
  */
---
+++ webapp/Exception_handling.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" errorPage="error_page.jsp"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%= 9/0 %>
+</body>
+</html>
