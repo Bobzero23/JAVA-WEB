8e173a6506c78c3eba42db8a692675895975c34e
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-05 08:40:11 +0300

    COMMIT 0840-5

--- src/main/java/jsp/Notes.java
+++ src/main/java/jsp/Notes.java
@@ -2,7 +2,22 @@
  * 
  * it makes easier to work with begger and complex web pages
  * 
- * DIFFERENCE BETWEEN JPS AND SERVLET
+ * DIFFERENCE BETWEEN JSP AND SERVLET
  * in servlet you can write the html code inside the java code which might become super messy for bigger projects
  * in jsp we we write the java code inside the html code which is way more easier and efficient
+ * 
+ * JSP will give you the REQUESTS and RESPONSE objects so you dont have to initiate them
+ * alo jsp gives you the object of PRINTWRITER just say out.println() and you are good to go
+ * 
+ * 
+ *  TO WORK WITH JSP
+ *  simply create a jsp file
+ *  isnide it will be some html code if not write yourself damn it hhh
+ *  then you should write the java code inside this <% hereJavaCode %>
+ *  
+ *  TO CALL JSP FILE IN HTML FILE
+ *  simply in the FORM tag of html write the JSP file name and you are good to go
+ *  
+ *  
+ *  
  */
